df9202acd00ccd2876fb024bb86536ea2152daeb
XpLoDWilD <xplodgui@gmail.com>
2012-11-23 15:55:34 +0000

    Early 4.2 stuffEarly 4.2 stuff
    Change-Id: I6dda797083b6a70a6576b8e5838abcb9e5d60757

--- camerawrapper/CameraWrapper.cpp
+++ camerawrapper/CameraWrapper.cpp
@@ -99,7 +99,7 @@ static char * camera_fixup_getparams(int id, const char * settings)
     params.unflatten(android::String8(settings));
 
     // fix params here
-    params.set(android::CameraParameters::KEY_SUPPORTED_ISO_MODES, iso_values[id]);
+//    params.set(android::CameraParameters::KEY_SUPPORTED_ISO_MODES, iso_values[id]);
 
     android::String8 strParams = params.flatten();
     char *ret = strdup(strParams.string());
@@ -115,7 +115,7 @@ char * camera_fixup_setparams(int id, const char * settings)
 
     // fix params here
     if(params.get("iso")) {
-        const char* isoMode = params.get(android::CameraParameters::KEY_ISO_MODE);
+/*        const char* isoMode = params.get(android::CameraParameters::KEY_ISO_MODE);
         if(strcmp(isoMode, "ISO100") == 0)
             params.set(android::CameraParameters::KEY_ISO_MODE, "100");
         else if(strcmp(isoMode, "ISO200") == 0)
@@ -123,7 +123,7 @@ char * camera_fixup_setparams(int id, const char * settings)
         else if(strcmp(isoMode, "ISO400") == 0)
             params.set(android::CameraParameters::KEY_ISO_MODE, "400");
         else if(strcmp(isoMode, "ISO800") == 0)
-            params.set(android::CameraParameters::KEY_ISO_MODE, "800");
+            params.set(android::CameraParameters::KEY_ISO_MODE, "800"); */
     }
 
     android::String8 strParams = params.flatten();
