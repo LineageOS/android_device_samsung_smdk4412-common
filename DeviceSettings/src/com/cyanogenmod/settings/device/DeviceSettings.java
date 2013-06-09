cd02df104333dd13d3f14b2a9cc41b558fc48fea
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2013-06-09 17:25:19 +0200

    smdk4412: advanced settings: add spen support back
    Change-Id: Ia7243737261bf63e406cd550d417f80b0f9a92cf

--- DeviceSettings/src/com/cyanogenmod/settings/device/DeviceSettings.java
+++ DeviceSettings/src/com/cyanogenmod/settings/device/DeviceSettings.java
@@ -46,6 +46,8 @@ public class DeviceSettings extends FragmentActivity {
     public static final String KEY_HSPA = "hspa";
     public static final String KEY_VIBRATOR_INTENSITY = "vibrator_intensity";
     public static final String KEY_USE_DOCK_AUDIO = "dock_audio";
+    public static final String KEY_CATEGORY_SPEN = "category_spen";
+    public static final String KEY_SPEN_POWER_SAVING_MODE = "spen_power_saving"; 
 
     ViewPager mViewPager;
     TabsAdapter mTabsAdapter;
