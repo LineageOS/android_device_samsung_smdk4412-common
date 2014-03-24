7998a46db189e42ff48dfa19e3b421402b72343e
Dheeraj CVR <cvr.dheeraj@gmail.com>
2014-03-24 07:16:42 +0530

    smdk4412-common: camera: release face detection memory when capture stops
    Change-Id: I6724ce6dde3c4b6242a06b73808905a6de094d4a

--- camera/exynos_camera.c
+++ camera/exynos_camera.c
@@ -2114,6 +2114,11 @@ int exynos_camera_capture_start(struct exynos_camera *exynos_camera)
 	goto complete;
 
 error:
+	if (exynos_camera->face_data != NULL && exynos_camera->face_data->release != NULL) {
+		exynos_camera->face_data->release(exynos_camera->face_data);
+		exynos_camera->face_data = NULL;
+	}
+
 	if (exynos_camera->capture_memory != NULL && exynos_camera->capture_memory->release != NULL) {
 		exynos_camera->capture_memory->release(exynos_camera->capture_memory);
 		exynos_camera->capture_memory = NULL;
@@ -2156,6 +2161,11 @@ void exynos_camera_capture_stop(struct exynos_camera *exynos_camera)
 	rc = exynos_v4l2_streamoff_cap(exynos_camera, 0);
 	if (rc < 0) {
 		ALOGE("%s: Unable to stop stream", __func__);
+	}
+
+	if (exynos_camera->face_data != NULL && exynos_camera->face_data->release != NULL) {
+		exynos_camera->face_data->release(exynos_camera->face_data);
+		exynos_camera->face_data = NULL;
 	}
 
 	if (exynos_camera->capture_memory != NULL && exynos_camera->capture_memory->release != NULL) {
