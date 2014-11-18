c75d5293a570c41ab02bd2eed468381ff819e16a
Howard Su <howard0su@gmail.com>
2014-11-18 09:09:44 +0000

    media: add google codec.
    Change-Id: I8542d1db999dd08ad4c2cb36cc65c3d9f5a22406

--- common.mk
+++ common.mk
@@ -102,7 +102,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
-    $(COMMON_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml
+    $(COMMON_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
 # Filesystem management tools
 PRODUCT_PACKAGES += \
