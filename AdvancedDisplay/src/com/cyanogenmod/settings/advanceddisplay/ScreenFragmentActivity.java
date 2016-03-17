97a84624bb3b616a0e4da5e04d3ec01f3950751f
Simon Shields <keepcalm444@gmail.com>
2016-03-17 22:38:29 +1100

    AdvancedDisplay: fix up for M
    Change-Id: I97a6a87956edaddfda60bd6496c719412d3a6ba9

--- AdvancedDisplay/src/com/cyanogenmod/settings/advanceddisplay/ScreenFragmentActivity.java
+++ AdvancedDisplay/src/com/cyanogenmod/settings/advanceddisplay/ScreenFragmentActivity.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.settings.device;
+package com.cyanogenmod.settings.advanceddisplay;
 
 import android.content.Context;
 import android.content.res.Resources;
@@ -30,7 +30,7 @@ import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
 import android.util.Log;
 
-import com.cyanogenmod.settings.device.R;
+import com.cyanogenmod.settings.advanceddisplay.R;
 
 public class ScreenFragmentActivity extends PreferenceFragment {
 
