466dfcd4a8c92a3d8cfb225ac6849764f6d863ec
Simon Shields <keepcalm444@gmail.com>
2016-03-17 22:38:29 +1100

    AdvancedDisplay: fix up for M
    * use EXTRA_SETTINGS intent
    * use material light to match settings
    * remove unused strings + fix string capitalisation
    
    Change-Id: I97a6a87956edaddfda60bd6496c719412d3a6ba9

--- AdvancedDisplay/src/com/cyanogenmod/settings/advanceddisplay/DisplaySettings.java
+++ AdvancedDisplay/src/com/cyanogenmod/settings/advanceddisplay/DisplaySettings.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.settings.device;
+package com.cyanogenmod.settings.advanceddisplay;
 
 import android.app.ActionBar;
 import android.app.ActionBar.Tab;
@@ -28,14 +28,14 @@ import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.MenuItem;
 
-import com.cyanogenmod.settings.device.R;
+import com.cyanogenmod.settings.advanceddisplay.R;
 
 import java.util.ArrayList;
 
 public class DisplaySettings extends FragmentActivity {
 
-    public static final String SHARED_PREFERENCES_BASENAME = "com.cyanogenmod.settings.device";
-    public static final String ACTION_UPDATE_PREFERENCES = "com.cyanogenmod.settings.device.UPDATE";
+    public static final String SHARED_PREFERENCES_BASENAME = "com.cyanogenmod.settings.advanceddisplay";
+    public static final String ACTION_UPDATE_PREFERENCES = "com.cyanogenmod.settings.advanceddisplay.UPDATE";
     public static final String KEY_CABC = "cabc";
     public static final String KEY_MDNIE_SCENARIO = "mdnie_scenario";
     public static final String KEY_MDNIE_MODE = "mdnie_mode";
