9136d0d7d3643a8c08b3a98f3e394d7514fff839
Simon Shields <keepcalm444@gmail.com>
2016-04-22 02:40:38 +1000

    smdk4412: update extract-files and proprietary-files
    libump/libfimc are no longer blobs
    
    Change-Id: I3251f759b914652620fd80ffb80198974e6fed44

--- extract-files.sh
+++ extract-files.sh
@@ -56,7 +56,6 @@ done
 LOCAL_PATH := vendor/samsung/smdk4412-common
 
 PRODUCT_PACKAGES += \\
-    libTVOut \\
     libUMP \\
     libfimc
 
@@ -99,26 +98,6 @@ include \$(CLEAR_VARS)
 LOCAL_MODULE := libTVOut
 LOCAL_MODULE_OWNER := samsung
 LOCAL_SRC_FILES := system/lib/libTVOut.so
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_PATH := \$(TARGET_OUT)/lib
-include \$(BUILD_PREBUILT)
-
-include \$(CLEAR_VARS)
-LOCAL_MODULE := libUMP
-LOCAL_MODULE_OWNER := samsung
-LOCAL_SRC_FILES := system/lib/libUMP.so
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_PATH := \$(TARGET_OUT)/lib
-include \$(BUILD_PREBUILT)
-
-include \$(CLEAR_VARS)
-LOCAL_MODULE := libfimc
-LOCAL_MODULE_OWNER := samsung
-LOCAL_SRC_FILES := system/lib/libfimc.so
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
