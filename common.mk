cd314179de4d3980f63fe04dad2af049f8542a4e
Simon Shields <keepcalm444@gmail.com>
2016-08-28 00:58:43 +1000

    smdk4412: remove unnecessary inherit-products
    Change-Id: I247b752af742322f4c38a3bce589f283cc7de7b2

--- common.mk
+++ common.mk
@@ -20,11 +20,6 @@ DEVICE_PACKAGE_OVERLAYS := $(COMMON_PATH)/overlay
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-# The gps config appropriate for this device
-$(call inherit-product, device/common/gps/gps_us_supl.mk)
-
 # Init files
 PRODUCT_COPY_FILES := \
     $(COMMON_PATH)/rootdir/init.smdk4x12.rc:root/init.smdk4x12.rc \
