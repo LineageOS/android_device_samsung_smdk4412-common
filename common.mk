a7dd296fb4e4eef913c9b5d4f440f39519fcf515
Simon Shields <keepcalm444@gmail.com>
2016-12-03 13:10:28 +1100

    smdk4412: remove unneeded netflix hack
    * netflix doesn't support the preference we were setting anymore, so
    let's just stop doing this
    
    Change-Id: I5040f85418499b8e4502f1573e8b78af9221e202

--- common.mk
+++ common.mk
@@ -38,10 +38,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/80cfw:system/etc/init.d/80cfw
 
-# Netflix hack
-PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/98netflix:system/etc/init.d/98netflix
-
 # Wifi
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
