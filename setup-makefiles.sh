8189edc0a7083c1fbc866daa2ba28b99a167d597
Simon Shields <keepcalm444@gmail.com>
2016-12-04 11:20:18 +1100

    smdk4412: switch to generic extract-utils
    Change-Id: I3e120bd012941c8e1e8220775057f92d9149f95b

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -30,25 +30,12 @@ if [ ! -f "$HELPER" ]; then
 fi
 . "$HELPER"
 
-if [ $# -eq 0 ]; then
-    SRC=adb
-else
-    if [ $# -eq 1 ]; then
-        SRC=$1
-    else
-        echo "$0: bad number of arguments"
-        echo ""
-        echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
-        echo ""
-        echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
-        echo "the device using adb pull."
-        exit 1
-    fi
-fi
-
 # Initialize the helper for common device
 setup_vendor "$DEVICE_COMMON" "$VENDOR" "$CM_ROOT" true
 
-extract "$MY_DIR"/proprietary-files.txt "$SRC"
+# Copyright headers and common guards
+write_headers "i9300 i9305 n7100 n8000 n8013 n8020 t0lte t0lteatt t0ltetmo i605 l900 r950 i925 n5100 n5110 n5120"
 
-"$MY_DIR"/setup-makefiles.sh
+write_makefiles "$MY_DIR"/proprietary-files.txt
+
+write_footers
