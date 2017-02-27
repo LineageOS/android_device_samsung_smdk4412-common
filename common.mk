d201b0c35d0c0f98baa7f75836a7b57c8eea3334
filiprrs <filiprrs@gmail.com>
2017-02-28 00:02:12 +0100

    smdk4412: DNM: Add Ambient Display related commits
    Taken from tilaksidduram github.
    
    Change-Id: I02cafa73589f418d16c497094b9fd893ed8d80a0
    Signed-off-by: filiprrs <filiprrs@gmail.com>

--- common.mk
+++ common.mk
@@ -99,6 +99,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
+PRODUCT_PACKAGES += \
+    SamsungDoze
+
 # Filesystem management tools
 PRODUCT_PACKAGES += \
     make_ext4fs \
