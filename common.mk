b629087af249e33e2b1cedc097c6f41c61c28a3d
filiprrs <filiprrs@gmail.com>
2017-02-27 23:05:00 +0100

    smdk4412: DNM: Try to deal with GPU Glitches
    UI issues on t0lte
    
    Change-Id: Ib1e8ba51f69acfaf8d6b7a1789b8605abda6679a
    Signed-off-by: filiprrs <filiprrs@gmail.com>

--- common.mk
+++ common.mk
@@ -165,8 +165,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.zygote.disable_gl_preload=1 \
     ro.opengles.version=131072 \
-    ro.bq.gpu_to_cpu_unsupported=1 \
-    debug.hwui.render_dirty_regions=false
 
 PRODUCT_TAGS += dalvik.gc.type-precise
 
